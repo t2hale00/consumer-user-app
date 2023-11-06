ada3eec1d0b6778b16a10f67ad8ca624b121ceae
Lea <112546991+t2hale00@users.noreply.github.com>
2023-11-06 22:40:09 +0200

    Changed app.css

--- client/consumer-user/src/App.js
+++ client/consumer-user/src/App.js
@@ -1,31 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // Use BrowserRouter as Router
-import './App.css';
+import './App.css'; // You may need to adjust the path based on your project structure
 import SignIn from './Signin'; // Import the SignIn component
 import SignUp from './Signup'; // Import the SignUp component
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1 style={{ fontSize: '2rem' }}>Paketti</h1>
-          <div className="buttons">
-            <Link to="/signup">
-              <button>Sign Up</button>
-            </Link>
-            <Link to="/signin">
-              <button>Sign In</button>
-            </Link>
-          </div>
-        </header>
-
-        <Routes>
-          <Route path="/signup" element={<SignUp />} /> {/* Use 'element' instead of 'component' */}
-          <Route path="/signin" element={<SignIn />} /> {/* Use 'element' instead of 'component' */}
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <h1 style={{ fontSize: '2rem' }}>Paketti</h1>
+        <div className="buttons">
+          <SignUp />
+          <SignIn />
+        </div>
+      </header>
+    </div>
   );
 }
 
