003b04ba10772637597b74682baa10be524505ae
Lea <112546991+t2hale00@users.noreply.github.com>
2023-11-07 23:02:27 +0200

    added cors, deleted sequelize

--- server/index.js
+++ server/index.js
@@ -2,10 +2,27 @@ const express   = require('express');
 const app       = express();
 const port      = 3000;
 
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World Wide Web!');
 }); 
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+
+
+//route to adding new customer user
+app.post('/newscustomeruser', (req,res) => {
+    res.send ('Hello new user!')
 });
+
+//route to get all customer user parcels
+app.get('/allparcels', (req,res) => {
+    res.send ('All parcels')
+});
+
+//search customer parcels by customer username and parcel id
+
+
+
